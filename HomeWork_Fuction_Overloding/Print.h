808cfc3778288e02b75c7e55bd1e2dc744b6e967
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2021-11-07 20:15:30 +0600

    templated_split DONE

--- HomeWork_Fuction_Overloding/Print.h
+++ HomeWork_Fuction_Overloding/Print.h
@@ -1,7 +1,7 @@
-#pragma once
+п»ї#pragma once
 #include"stdafx.h"
 #include"Arrs.h"
 template<typename T> void Print(T arr[], const unsigned int N);
 template<typename T> void Print(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS);
-template<typename T> void ReversePrint(T arr[], const unsigned int N); //Выводит массив в обратном направлении;
-template<typename T> void ReversePrint(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS); //Выводит массив в обратном направлении;
+template<typename T> void ReversePrint(T arr[], const unsigned int N); //Р’С‹РІРѕРґРёС‚ РјР°СЃСЃРёРІ РІ РѕР±СЂР°С‚РЅРѕРј РЅР°РїСЂР°РІР»РµРЅРёРё;
+template<typename T> void ReversePrint(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS); //Р’С‹РІРѕРґРёС‚ РјР°СЃСЃРёРІ РІ РѕР±СЂР°С‚РЅРѕРј РЅР°РїСЂР°РІР»РµРЅРёРё;
