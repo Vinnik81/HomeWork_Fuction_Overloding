79cabdba24881180e56f104f5e10b3cd90e1f853
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2021-11-07 20:18:19 +0600

    templated_split DONE

--- HomeWork_Fuction_Overloding/Shift.h
+++ HomeWork_Fuction_Overloding/Shift.h
@@ -1,7 +1,7 @@
-#pragma once
+п»ї#pragma once
 #include"stdafx.h"
 #include"Const.h"
-template<typename T> void shiftLeft(T arr[], const unsigned int N, int k); //Выполняет циклический сдвиг массива влево, на заданное число элементов; 
+template<typename T> void shiftLeft(T arr[], const unsigned int N, int k); //Р’С‹РїРѕР»РЅСЏРµС‚ С†РёРєР»РёС‡РµСЃРєРёР№ СЃРґРІРёРі РјР°СЃСЃРёРІР° РІР»РµРІРѕ, РЅР° Р·Р°РґР°РЅРЅРѕРµ С‡РёСЃР»Рѕ СЌР»РµРјРµРЅС‚РѕРІ; 
 template<typename T> void shiftLeft(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS, int k);
-template<typename T> void shiftRight(T arr[], const unsigned int N, int k); //Выполняет циклический сдвиг массива вправо, на заданное число элементов;
+template<typename T> void shiftRight(T arr[], const unsigned int N, int k); //Р’С‹РїРѕР»РЅСЏРµС‚ С†РёРєР»РёС‡РµСЃРєРёР№ СЃРґРІРёРі РјР°СЃСЃРёРІР° РІРїСЂР°РІРѕ, РЅР° Р·Р°РґР°РЅРЅРѕРµ С‡РёСЃР»Рѕ СЌР»РµРјРµРЅС‚РѕРІ;
 template<typename T> void shiftRight(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS, int k);
